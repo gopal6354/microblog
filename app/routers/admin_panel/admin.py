f6664f5e5e38420fd5a35979f65329dc1ab5be4f
Gopal Patilgit config --global user.email gpl@taglineinfotch.com <gpl@taglineinfotech.com>
2026-07-22 09:31:04 +0530

    feat: Implement admin panel and nagivation

--- app/routers/admin_panel/admin.py
+++ app/routers/admin_panel/admin.py
@@ -8,3 +8,31 @@ router = APIRouter()
 @router.get("/admin-dashboard")
 def admin_dashboard_page(request: Request):
     return templates.TemplateResponse(request=request, name="admin/dashboard.html")
+
+
+@router.get("/admin/users")
+def admin_users_page(requst: Request):
+    return templates.TemplateResponse(
+        request=requst, name="admin/components/users.html"
+    )
+
+
+@router.get("/admin/blogs")
+def admin_blogs_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="admin/components/blogs.html"
+    )
+
+
+@router.get("/admin/reports")
+def admin_reports_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="admin/components/reports.html"
+    )
+
+
+@router.get("/admin/activation-requests")
+def admin_activation_request_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="admin/components/activation_request.html"
+    )
